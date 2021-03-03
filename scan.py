e694e83128c37cece912aee29ce19defe894ad91
Jeroen Delcour <jeroendelcour@gmail.com>
2021-03-03 22:23:05 +0100

    x, y, z of laser points in millimeters per frame, 3D scatterplot

--- scan.py
+++ scan.py
@@ -46,22 +46,20 @@ cap = cv2.VideoCapture("video.h264")
 
 shifts = []
 
-for i in range(66):
-    success, image = cap.read()
 while True:
     success, image = cap.read()
     if not success:
         break
 
     if np.median(image) < 10:  # short exposure image
-        gray = image[:,:,2].astype(float) - np.mean(image[:,:,:2], axis=-1)
+        gray = image[:, :, 2].astype(float) - np.mean(image[:, :, :2], axis=-1)
         gray -= gray.min()
         gray /= gray.max() / 255
         gray = np.clip(gray, 0, 255)
         gray = gray.astype(np.uint8)
-        image[:,:,0] = gray
-        image[:,:,1] = gray
-        image[:,:,2] = gray
+        image[:, :, 0] = gray
+        image[:, :, 1] = gray
+        image[:, :, 2] = gray
         frame_shifts = np.argmax(gray, axis=0)
         for i in range(len(frame_shifts)):
             y = frame_shifts[i]
@@ -83,21 +81,81 @@ while True:
 
     cv2.imshow("", image)
     # key = cv2.waitKey(1000//60)
-    key = cv2.waitKey(0)
+    key = cv2.waitKey(1)
     if key == 27:  # ESC pressed
         break
 
 shifts = np.array(shifts)
 shifts = shifts.astype(float)
 shifts[shifts == 0] = np.nan
+shifts -= 0.5*720  # measure from optical center
+shifts *= -1  # flip
+shifts *= 0.001121951  # pixels to mm
 
-plt.imshow(shifts, cmap="magma")
+# def line(p1, p2):
+#     A = (p1[1] - p2[1])
+#     B = (p2[0] - p1[0])
+#     C = (p1[0]*p2[1] - p2[0]*p1[1])
+#     return A, B, -C
+
+# def intersection(L1, L2):
+#     D  = L1[0] * L2[1] - L1[1] * L2[0]
+#     Dx = L1[2] * L2[1] - L1[1] * L2[2]
+#     Dy = L1[0] * L2[2] - L1[2] * L2[0]
+#     if D != 0:
+#         x = Dx / D
+#         y = Dy / D
+#         return x,y
+#     else:
+#         return False
+
+# laser_origin = [-baseline, 0]
+# laser_vector = [-baseline + np.cos(angle), 0 + np.sin(angle)]
+# camera_origin = [0, 0]
+# depths = []
+# laser_line = line(laser_origin, laser_vector)
+# for shift in shifts:
+#     d = []
+#     for y in shift:
+#         shift_vector = [y, focal_length]
+#         camera_line = line(camera_origin, shift_vector)
+#         x, y = intersection(laser_line, camera_line)
+#         depth = y
+#         d.append(depth)
+#         if not depth:
+#             d[-1] = np.nan
+#     depths.append(d)
+# depths = np.array(depths)
+
+angle = np.deg2rad(70)
+baseline = 118.478  # mm
+focal_length = 3.04  # mm
+z = (baseline * focal_length * np.tan(angle)) / (focal_length - shifts * np.tan(angle))
+y = z * shifts / focal_length  # similar triangles
+x = np.zeros_like(shifts)
+x[:] = np.arange(shifts.shape[1]) - 0.5 * shifts.shape[1]  # in pixels
+x *= 0.001121951  # pixels to mm
+# x = np.arange(len(shifts)) - 0.5 * len(shifts)  # in pixels
+x = z * x / focal_length
+depths = z
+
+# assume camera moves steadily along Y axis
+for i in range(len(y)):
+    y[i] -= i
+
+
+plt.imshow(depths, cmap="magma")
 plt.show()
 
-# fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 # X = range(shifts.shape[1])
 # Y = range(shifts.shape[0])
 # X, Y = np.meshgrid(X, Y)
 # Z = shifts
 # ax.plot_surface(X, Y, Z, antialiased=False)
-# plt.show()
+
+# plot a random sample to keep framerate managable
+every = 20
+ax.scatter(x[:, ::every].flatten(), y[:, ::every].flatten(), -z[:, ::every].flatten(), c=-z[:, ::every].flatten())
+plt.show()
+#
