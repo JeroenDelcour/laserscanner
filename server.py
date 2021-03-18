0e0cd62ec1bac9c8ed966bf5e4774fce101b534a
Jeroen Delcour <jeroendelcour@gmail.com>
2021-03-18 08:29:43 +0100

    fixed coordinate system transform from opencv to three js

--- server.py
+++ server.py
@@ -6,6 +6,11 @@ import json
 import cv2
 import cv2.aruco
 import math
+from pprint import pprint
+
+
+def rodrigues_rotation(v, k, theta):
+    return v*np.cos(theta) + (np.cross(k,v)*np.sin(theta)) + k*(np.dot(k,v))*(1.0-np.cos(theta))
 
 
 def rotationMatrixToEulerAngles(R):
@@ -22,31 +27,13 @@ def rotationMatrixToEulerAngles(R):
     return np.array([x, y, z])
 
 
-# async def position(websocket, path):
-#     while True:
-#         message = {
-#             "camera": {
-#                 "position": {
-#                     "x": random.random(),
-#                     "y": random.random(),
-#                     "z": random.random(),
-#                 },
-#                 "rotation": {
-#                     "x": 0,
-#                     "y": 0,
-#                     "z": 0,
-#                 }
-#             }
-#         }
-#         await websocket.send(json.dumps(message))
-#         await asyncio.sleep(1)
-
 async def position(websocket, path):
-    cap = cv2.VideoCapture("/home/jeroen/Downloads/VID_20210316_144205.mp4")
+    # cap = cv2.VideoCapture("/home/jeroen/Downloads/VID_20210316_144205.mp4")
+    cap = cv2.VideoCapture("http://192.168.178.10:8080/video")
 
-    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_100)
+    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_100)
 
-    fx = fy = width = 1280
+    fx = fy = width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     cx = width / 2
     cy = width / 2
     intrinsic_matrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
@@ -60,37 +47,68 @@ async def position(websocket, path):
             break
 
         corners, ids, rejected = cv2.aruco.detectMarkers(image, aruco_dict)
-        cv2.aruco.drawDetectedMarkers(image, corners, ids)
-        cv2.imshow("", image)
-        cv2.waitKey(1)
+        image = cv2.aruco.drawDetectedMarkers(image, corners, ids)
 
         if not corners:
+            cv2.imshow("", image)
+            cv2.waitKey(1)
             continue
         rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(
-            corners, markerLength=0.12, cameraMatrix=intrinsic_matrix, distCoeffs=dist_coeffs)
-        tvecs = tvecs[0]
-        rvecs = rvecs[0]
+            corners, markerLength=0.100, cameraMatrix=intrinsic_matrix, distCoeffs=dist_coeffs)
+        for rvec, tvec, id_ in zip(rvecs, tvecs, ids):
+            if id_ == 77:
+                image = cv2.aruco.drawAxis(image, intrinsic_matrix, dist_coeffs, rvec, tvec, 0.1)
+
+        cv2.imshow("", image)
+        cv2.waitKey(1)
         
-        r_euler_angles = [rotationMatrixToEulerAngles(cv2.Rodrigues(rvec)[0].T) for rvec in rvecs]
-        
+        # r_euler_angles = [rotationMatrixToEulerAngles(cv2.Rodrigues(rvec[0])[0].T) for rvec in rvecs]
+
+        try:
+            target_idx = list(ids).index(77)
+        except ValueError:
+            continue
+
+        T = tvecs[target_idx][0]
+        R = rvecs[target_idx][0]
+
+        # invert
+        R = -R
+        theta = np.linalg.norm(R)
+        Rnorm = R / theta  # normalized
+        T = rodrigues_rotation(T, Rnorm, theta)
+
+        # rotate 180 degrees around X axis
+        # T = rodrigues_rotation(T, np.array([1, 0, 0]), -np.pi)
+
+        # alternative invert
+        # R, _ = cv2.Rodrigues(R)
+        # R = R.T
+        # R, _ = cv2.Rodrigues(R)
+        # R = R[:, 0]
+        # T = -T
+        # theta = np.linalg.norm(R)
+        # Rnorm = R / theta
+
         message = {
             "camera": {
                 "position": {
-                    "x": -tvecs[0][0],
-                    "y": -tvecs[0][1],
-                    "z": -tvecs[0][2],
+                    "x": T[0],
+                    "y": T[1],
+                    "z": T[2],
                 },
                 "rotation": {
-                    "x": r_euler_angles[0][0],
-                    "y": r_euler_angles[0][1],
-                    "z": r_euler_angles[0][2],
+                    "x": Rnorm[0],
+                    "y": Rnorm[1],
+                    "z": Rnorm[2],
+                    "angle": theta,
                 }
             }
         }
 
         await websocket.send(json.dumps(message))
 
-        # await asyncio.sleep(1/30)
+        await asyncio.sleep(1/30)
 
 
 async def main():
