d6992fce3f4b34c2e78bb4c38c6c03c95dfba541
Jeroen Delcour <jeroendelcour@gmail.com>
2021-05-19 22:43:34 +0200

    some streaming improvements and optimizations in image processing

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import numpy as np
 import asyncio
 import random
 import websockets
-import json
+import ujson as json
 import cv2
 import cv2.aruco
 import math
@@ -13,11 +13,13 @@ ANGLE = np.deg2rad(75)
 BASELINE = 118.478e-3  # meters
 FOCAL_LENGTH = 3.04e-3  # meters
 # PIXEL_SIZE = 2 * 1.12e-6  # meters
-PIXEL_SIZE = 3.68e-3 / 3280
+# VERTICAL_RESOLUTION = 1232
+# HORIZONTAL_RESOLUTION = 1640
 VERTICAL_RESOLUTION = 1232
 HORIZONTAL_RESOLUTION = 1640
+PIXEL_SIZE = 3.68e-3 / HORIZONTAL_RESOLUTION
 fx = fy = 1355
-cx, cy = 820, 616
+cx, cy = HORIZONTAL_RESOLUTION // 2, VERTICAL_RESOLUTION // 2
 INTRINSIC_MATRIX = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]], dtype=np.float32)
 DIST_COEFFS = np.array([0.22128342058032355,
                         -0.5663376863990286,
@@ -31,10 +33,25 @@ _points_arange = np.arange(len(points))
 screen_points = np.zeros(shape=(len(points), 2), dtype=np.float32)
 screen_points[:, 0] = _points_arange
 
+aruco_parameters = cv2.aruco.DetectorParameters_create()
+aruco_parameters.errorCorrectionRate = 0  # default is 0.6
+aruco_parameters.maxErroneousBitsInBorderRate = 0  # default is 0.35
+# aruco_parameters.perspectiveRemovePixelPerCell = 4  # default is 4
+# aruco_parameters.perspectiveRemoveIgnoredMarginPerCell = 0.13  # default is 0.13
+# aruco_parameters.adaptiveThreshWinSizeMin = 3  # default is 3
+# aruco_parameters.adaptiveThreshWinSizeMax = 23  # default is 23
+# aruco_parameters.adaptiveThreshWinSizeStep = 10
+# aruco_parameters.adaptiveThreshConstant = 7
+aruco_parameters.minMarkerPerimeterRate = 0.03  # default is 0.03, 0.15 works well
+# aruco_parameters.maxMarkerPerimeterRate = 4.0
+# aruco_parameters.minCornerDistanceRate = 0.05
+# aruco_parameters.polygonalApproxAccuracyRate = 0.05  # default is 0.05
+
 
 def detect_laser(image, lower_threshold=10):
-    image = image.astype(float)
-    gray = image[:, :, 2] - 0.5 * (image[:, :, 0] + image[:, :, 1])
+    # image = image.astype(float)
+    # gray = image[:, :, 2] - 0.5 * (image[:, :, 0] + image[:, :, 1])
+    gray = image[:, :, 2]
     # gray -= np.mean(image[:, :, :2], axis=-1)
     # gray -= gray.min()
     # gray /= gray.max() / 255
@@ -53,7 +70,8 @@ def get_laserpoints(shifts):
     shifts = shifts.astype(float)
     shifts[shifts == -1] = np.nan
     screen_points[:, 1] = shifts
-    undistorted_screen_points = cv2.undistortPoints(screen_points[np.newaxis, :], INTRINSIC_MATRIX, DIST_COEFFS, P=INTRINSIC_MATRIX)
+    undistorted_screen_points = cv2.undistortPoints(
+        screen_points[np.newaxis, :], INTRINSIC_MATRIX, DIST_COEFFS, P=INTRINSIC_MATRIX)
     undistorted_screen_points = undistorted_screen_points.squeeze()
     shifts[:] = undistorted_screen_points[:, 1]
     shifts -= 0.5*VERTICAL_RESOLUTION  # measure from optical center
@@ -73,8 +91,12 @@ def rodrigues_rotation(v, k, theta):
     return v*np.cos(theta) + (np.cross(k, v)*np.sin(theta)) + k*(np.dot(k, v))*(1.0-np.cos(theta))
 
 
+@profile
 async def position(websocket, path):
-    # cap = cv2.VideoCapture("test.h264")
+    # cap = cv2.VideoCapture(2)
+    # cap.set(3, HORIZONTAL_RESOLUTION)  # width
+    # cap.set(4, VERTICAL_RESOLUTION)  # height
+    # cap.set(5, 30)  # framerate
     cap = cv2.VideoCapture("http://raspberrypi.local:8000/stream.mjpg")
 
     aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
@@ -91,92 +113,102 @@ async def position(websocket, path):
     prev_rvec, prev_tvec = None, None
     success, image = cap.read()
     vis = np.zeros_like(image)
+    T = [0., 0., 0.]
+    Rnorm = [1., 0., 0.]
+    theta = 0.
+    points = np.array([0.]*HORIZONTAL_RESOLUTION)
     while True:
         success, image = cap.read()
         if not success:
             break
         vis[:] = image
 
-        shifts = detect_laser(image)
-        vis[shifts, np.arange(image.shape[1])] = (0, 255, 0)
-        points = get_laserpoints(shifts)
+        sample = image[:, HORIZONTAL_RESOLUTION//2].mean()
+        if sample < 10:
+            shifts = detect_laser(image)
+            vis[shifts, np.arange(image.shape[1])] = (0, 255, 0)
+            points = get_laserpoints(shifts)
         # points = np.zeros(shape=(len(shifts), 3))
         # points[:, 0] = np.arange(len(shifts))  # x
         # points[:, 2] = 0.3  # z (depth)
 
-        corners, ids, rejected = cv2.aruco.detectMarkers(image, aruco_dict)
-        # image = cv2.aruco.drawDetectedMarkers(image, corners, ids)
-        corners, ids, rejected, recovered = cv2.aruco.refineDetectedMarkers(
-            image, board, corners, ids, rejected, cameraMatrix=INTRINSIC_MATRIX, distCoeffs=DIST_COEFFS
-        )
-        if corners:
-            retval, corners, ids = cv2.aruco.interpolateCornersCharuco(
-                corners, ids, image, board
+        else:
+            scale = 0.5
+            corners, ids, rejected = cv2.aruco.detectMarkers(
+                cv2.resize(image, dsize=None, fx=scale, fy=scale), aruco_dict)
+            corners = [c / scale for c in corners]
+            # image = cv2.aruco.drawDetectedMarkers(image, corners, ids)
+            corners, ids, rejected, recovered = cv2.aruco.refineDetectedMarkers(
+                image, board, corners, ids, rejected, cameraMatrix=INTRINSIC_MATRIX, distCoeffs=DIST_COEFFS
             )
-            vis = cv2.aruco.drawDetectedCornersCharuco(vis, corners, ids)
+            if corners:
+                retval, corners, ids = cv2.aruco.interpolateCornersCharuco(
+                    corners, ids, image, board
+                )
+                vis = cv2.aruco.drawDetectedCornersCharuco(vis, corners, ids)
 
-            # rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(
-            #     corners, markerLength=0.05, cameraMatrix=intrinsic_matrix, distCoeffs=dist_coeffs)
-            # for rvec, tvec, id_ in zip(rvecs, tvecs, ids):
-            #     if id_ == 77:
-            #         image = cv2.aruco.drawAxis(image, intrinsic_matrix, dist_coeffs, rvec, tvec, 0.1)
+                # rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(
+                #     corners, markerLength=0.05, cameraMatrix=intrinsic_matrix, distCoeffs=dist_coeffs)
+                # for rvec, tvec, id_ in zip(rvecs, tvecs, ids):
+                #     if id_ == 77:
+                #         image = cv2.aruco.drawAxis(image, intrinsic_matrix, dist_coeffs, rvec, tvec, 0.1)
 
-            success, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(
-                corners, ids, board, INTRINSIC_MATRIX, DIST_COEFFS, rvec=prev_rvec, tvec=prev_tvec, useExtrinsicGuess=prev_tvec is not None)
+                success, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(
+                    corners, ids, board, INTRINSIC_MATRIX, DIST_COEFFS, rvec=prev_rvec, tvec=prev_tvec, useExtrinsicGuess=prev_tvec is not None)
 
-            # gray = image[:, :, 2].astype(float) - np.mean(image[:, :, :2], axis=-1)
-            # gray -= gray.min()
-            # gray /= gray.max() / 255
-            # gray = np.clip(gray, 0, 255)
-            # gray = gray.astype(np.uint8)
-            # vis = np.hstack([vis, cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)])
+                # gray = image[:, :, 2].astype(float) - np.mean(image[:, :, :2], axis=-1)
+                # gray -= gray.min()
+                # gray /= gray.max() / 255
+                # gray = np.clip(gray, 0, 255)
+                # gray = gray.astype(np.uint8)
+                # vis = np.hstack([vis, cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)])
 
-            if not success:
-                prev_rvec = None
-                prev_tvec = None
-                continue
-            prev_rvec = rvec
-            prev_tvec = tvec
+                if not success:
+                    prev_rvec = None
+                    prev_tvec = None
+                    continue
+                prev_rvec = rvec
+                prev_tvec = tvec
 
-            # try:
-            #     target_idx = list(ids).index(77)
-            # except ValueError:
-            #     continue
+                # try:
+                #     target_idx = list(ids).index(77)
+                # except ValueError:
+                #     continue
 
-            # T = tvecs[target_idx][0]
-            # R = rvecs[target_idx][0]
-            T = tvec[:, 0]
-            R = rvec[:, 0]
+                # T = tvecs[target_idx][0]
+                # R = rvecs[target_idx][0]
+                T = tvec[:, 0]
+                R = rvec[:, 0]
 
-            # invert
-            R = -R
-            theta = np.linalg.norm(R)
-            Rnorm = R / theta  # normalized
-            T = rodrigues_rotation(T, Rnorm, theta)
+                # invert
+                R = -R
+                theta = np.linalg.norm(R)
+                Rnorm = R / theta  # normalized
+                T = rodrigues_rotation(T, Rnorm, theta)
 
-            message = {
-                "camera": {
-                    "position": {
-                        "x": T[0],
-                        "y": T[1],
-                        "z": T[2],
-                    },
-                    "rotation": {
-                        "x": Rnorm[0],
-                        "y": Rnorm[1],
-                        "z": Rnorm[2],
-                        "angle": theta,
-                    }
+        message = {
+            "camera": {
+                "position": {
+                    "x": T[0],
+                    "y": T[1],
+                    "z": T[2],
                 },
-                "points": points.tolist()
-            }
+                "rotation": {
+                    "x": Rnorm[0],
+                    "y": Rnorm[1],
+                    "z": Rnorm[2],
+                    "angle": theta,
+                }
+            },
+            "points": points.tolist()
+        }
+        message = json.dumps(message)
+        await websocket.send(message)
 
-            await websocket.send(json.dumps(message))
+        # cv2.imshow("", vis)
+        # cv2.waitKey(1)
 
-        cv2.imshow("", vis)
-        cv2.waitKey(1)
-
-        await asyncio.sleep(1/30)
+        # await asyncio.sleep(1/30)
 
 
 async def main():
--- stream.py
+++ stream.py
@@ -1,22 +1,111 @@
-import socket
-import time
+import io
 import picamera
+import logging
+import socketserver
+from threading import Condition
+from http import server
+import gpiozero
+from time import sleep
 
-camera = picamera.PiCamera()
-camera.resolution = (1280, 720)
-camera.framerate = 60
+PAGE="""\
+<html>
+<head>
+<title>picamera MJPEG streaming demo</title>
+</head>
+<body>
+<h1>PiCamera MJPEG Streaming Demo</h1>
+<img src="stream.mjpg" width="1640" height="1232" />
+</body>
+</html>
+"""
 
-server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 8000))
-server_socket.listen(0)
+class StreamingOutput(object):
+    def __init__(self, camera, short_shutter=200, long_shutter="auto"):
+        self.camera = camera
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+        self._longshutter_mode = True
+        self.short_shutter = short_shutter
+        if long_shutter == "auto":
+            self.long_shutter = camera.exposure_speed
+        else:
+            self.long_shutter = long_shutter
 
-while True:
-    # Accept a single connection and make a file-like object out of it
-    with server_socket.accept()[0].makefile('wb') as connection:
-        camera.start_recording(connection, format='mjpeg')
-        while True:
-            pass
-    camera.stop_recording()
+    def write(self, buf):
+        if self._longshutter_mode:
+            self.camera.shutter_speed = self.long_shutter
+        else:
+            self.camera.shutter_speed = self.short_shutter
+        self._longshutter_mode = not self._longshutter_mode
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
 
-server_socket.close()
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+laser = gpiozero.LED(17)
+laser.on()
+
+with picamera.PiCamera(resolution='1640x1232', framerate=25, sensor_mode=4) as camera:
+    # camera.shutter_speed = 200
+    camera.iso = 800
+    sleep(2)  # let automatic gain settle
+    camera.exposure_mode = "off"
+    output = StreamingOutput(camera, long_shutter=20000)
+    camera.start_recording(output, format='mjpeg')
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        laser.off()
+        camera.stop_recording()
 
