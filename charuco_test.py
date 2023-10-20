11bef72634af676b085732ebd4d9904fb580c233
Jeroen Delcour <jeroendelcour@gmail.com>
2023-10-20 16:59:42 +0200

    refactor, complimentary filter for position

--- charuco_test.py
+++ charuco_test.py
@@ -3,10 +3,11 @@ import cv2
 import time
 import json
 import websockets
-import numpy as np
+from pprint import pprint
 
 from pyquaternion import Quaternion
 from picamera2 import Picamera2
+import numpy as np
 
 from MPU6050 import MPU6050
 
@@ -16,12 +17,12 @@ class IMU:
         device_address = 0x68
         # The offsets are different for each device and should be changed
         # accordingly using a calibration procedure
-        x_accel_offset = -2308
-        y_accel_offset = -1525
-        z_accel_offset = 2565
-        x_gyro_offset = 36
-        y_gyro_offset = 19
-        z_gyro_offset = -23
+        x_accel_offset = -2310
+        y_accel_offset = -1531
+        z_accel_offset = 2556
+        x_gyro_offset = 31
+        y_gyro_offset = 22
+        z_gyro_offset = -25
         self.mpu = MPU6050(
             i2c_bus,
             device_address,
@@ -71,126 +72,160 @@ class IMU:
         
         return quat, acceleration
 
+class Camera:
+    def __init__(self):
+        focal_length = 3.04e-3  # meters
+        vertical_resolution = 800
+        horizontal_resolution = 600
+        fx = fy = 1355 / 2
+        cx, cy = 0.5 * horizontal_resolution, 0.5 * vertical_resolution
+        self.camera_matrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]], dtype=np.float32)
+        self.distortion_coefficients = np.array(
+            [
+                0.22128342058032355,
+                -0.5663376863990286,
+                -0.0001804474513748153,
+                -0.001201953225667692,
+                0.2602535953452802,
+            ],
+            dtype=np.float32,
+        )
+
+
+        self.aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)
+        self.charuco_board = cv2.aruco.CharucoBoard_create(
+            squaresX=5, squaresY=7, squareLength=0.015, markerLength=0.011, dictionary=self.aruco_dict
+        )
+
+        self.tvec = None
+        self.rvec = None
+
+        # image = self.charuco_board.generateImage(outSize=(600, 500), marginSize=40, borderBits=1)
+        # cv2.imwrite("charuco_board.png", image)
+
+    def estimate_pose(self, image):
+        corners, ids, rejected = cv2.aruco.detectMarkers(image, dictionary=self.aruco_dict)
+        if len(corners) > 0:
+            retval, corners, ids = cv2.aruco.interpolateCornersCharuco(
+                corners, ids, image, self.charuco_board
+            )
+            success, self.rvec, self.tvec = cv2.aruco.estimatePoseCharucoBoard(
+                corners,
+                ids,
+                self.charuco_board,
+                self.camera_matrix,
+                self.distortion_coefficients,
+                rvec=self.rvec,
+                tvec=self.tvec,
+                useExtrinsicGuess=True if self.tvec is not None else False,
+            )
+            if success:
+                # position and quaternion of the board in the camera frame
+                angle = np.linalg.norm(self.rvec[:,0])
+                axis = self.rvec[:,0] / angle
+                quat_cam = Quaternion(axis=axis, radians=angle)
+                pos_cam = self.tvec[:,0]
+
+                # invert so we have the camera from the board's frame
+                quat_cam = quat_cam.inverse
+                pos_cam = -pos_cam
+
+                # rotate 90 degrees around X axis so Y is up instead of Z
+                quat_cam = Quaternion(axis=(1, 0, 0), degrees=-90) * quat_cam
+                pos_cam = quat_cam.rotate(pos_cam)
+
+                return True, pos_cam, quat_cam
+
+        # pose could not be estimated
+        return False, None, None
+
 
 async def sense(websocket):
-    focal_length = 3.04e-3  # meters
-    vertical_resolution = 800
-    horizontal_resolution = 600
-    fx = fy = 1355 / 2
-    cx, cy = 0.5 * horizontal_resolution, 0.5 * vertical_resolution
-    camera_matrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]], dtype=np.float32)
-    distortion_coefficients = np.array(
-        [
-            0.22128342058032355,
-            -0.5663376863990286,
-            -0.0001804474513748153,
-            -0.001201953225667692,
-            0.2602535953452802,
-        ],
-        dtype=np.float32,
-    )
-
-
-    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)
-    charuco_board = cv2.aruco.CharucoBoard_create(
-        squaresX=5, squaresY=7, squareLength=0.015, markerLength=0.011, dictionary=aruco_dict
-    )
-
-    # image = charuco_board.generateImage(outSize=(600, 500), marginSize=40, borderBits=1)
-    # cv2.imwrite("charuco_board.png", image)
-
+    # initialize PiCamera
     picam2 = Picamera2()
     config = picam2.create_video_configuration(main={"size": (800, 600), "format": "BGR888"})
-    # config = picam2.create_preview_configuration({"size": (800, 600), "format": "BGR888"})
     picam2.align_configuration(config)
     print(config["main"])
     picam2.configure(config)
     picam2.start()
 
+    camera = Camera()
     imu = IMU()
 
-    cam_quat = Quaternion()
-    tvec = np.zeros(shape=(3,1))
-
-    imu_correction = Quaternion()
-
-    ###
-    # x = x + v*dt + alpha*dk
-    # v = v + a*dt +  beta*dk
-    ###
-
-    q_correction = Quaternion()
+    # state
+    position = np.zeros(3)
+    velocity = np.zeros(3)
+    quat = Quaternion()
+    quat_correction = Quaternion()
+    quat_cam = Quaternion()
+    position_cam = np.zeros(3)
+    t_prev = time.time()
 
     while True:
-        imu_quat, imu_acceleration = imu.read()
-        imu_quat = q_correction * imu_quat
+        # update quaternion with IMU readings
+        quat, acceleration = imu.read()
+        quat = quat_correction * quat
 
-        # rotate acceleration vector from IMU frame to world frame
-        imu_acceleration = imu_quat.rotate(imu_acceleration)
+        # update timestep
+        now = time.time()
+        dt = now - t_prev
+        t_prev = now
+        print(f"dt: {dt}")
 
+        # update velocity and position with IMU readings
+        acceleration = quat.rotate(acceleration)  # rotate acceleration vector from IMU frame to world frame
+        velocity += acceleration * dt
+        position += velocity * dt
+
+        # apply correction from camera pose estimation
         image = picam2.capture_array("main")
+        success, new_position_cam, new_quat_cam = camera.estimate_pose(image)
+        if success:
+            position_cam = new_position_cam
+            quat_cam = new_quat_cam
 
-        corners, ids, rejected = cv2.aruco.detectMarkers(image, dictionary=aruco_dict)
-        if len(corners) > 0:
+            # orientation correction
+            gamma = 0.5
+            dq = quat_cam * quat.inverse
+            quat_correction = Quaternion(axis=dq.axis, angle=gamma * dq.angle) * quat_correction
 
-            retval, corners, ids = cv2.aruco.interpolateCornersCharuco(
-                corners, ids, image, charuco_board
-            )
-            success, rvec, new_tvec = cv2.aruco.estimatePoseCharucoBoard(
-                corners,
-                ids,
-                charuco_board,
-                camera_matrix,
-                distortion_coefficients,
-                rvec=None,
-                tvec=None,
-                useExtrinsicGuess=False,
-            )
-            if success:
-                # tvec and quat of the board in the camera frame
-                tvec = new_tvec
-                angle = np.linalg.norm(rvec[:,0])
-                axis = rvec[:,0] / angle
-                cam_quat = Quaternion(axis=axis, radians=angle)
-
-                # invert so we have the camera from the board's frame
-                cam_quat = cam_quat.inverse
-                tvec = -tvec
-
-                # rotate 90 degrees around X axis so Y is up instead of Z
-                dq = Quaternion(axis=(1, 0, 0), degrees=-90)
-                cam_quat = dq * cam_quat
-                tvec[:,0] = cam_quat.rotate(tvec[:,0])
-
-                # orientation correction
-                alpha = 0.5
-                dq = cam_quat * imu_quat.inverse
-                q_correction = Quaternion(axis=dq.axis, angle=alpha * dq.angle) * q_correction
+            # position and velocity correction
+            alpha = 0.2
+            beta = 0.2
+            correction = position_cam - position
+            velocity += beta * correction
+            position += alpha * correction
 
         message = {
+            "quaternion": {
+                "x": quat.x,
+                "y": quat.y,
+                "z": quat.z,
+                "w": quat.w,
+            },
             "cam_quaternion": {
-                "x": cam_quat.x,
-                "y": cam_quat.y,
-                "z": cam_quat.z,
-                "w": cam_quat.w,
+                "x": quat_cam.x,
+                "y": quat_cam.y,
+                "z": quat_cam.z,
+                "w": quat_cam.w,
             },
-            "imu_quaternion": {
-                "x": imu_quat.x,
-                "y": imu_quat.y,
-                "z": imu_quat.z,
-                "w": imu_quat.w,
+            "position": {
+                "x": position[0],
+                "y": position[1],
+                "z": position[2],
             },
-            "translation": {
-                "x": tvec[0, 0],
-                "y": tvec[1, 0],
-                "z": tvec[2, 0],
+            "velocity": {
+                "x": velocity[0],
+                "y": velocity[1],
+                "z": velocity[2],
             },
             "acceleration": {
-                "x": imu_acceleration[0],
-                "y": imu_acceleration[1],
-                "z": imu_acceleration[2],
+                "x": acceleration[0],
+                "y": acceleration[1],
+                "z": acceleration[2],
             }
         }
+        pprint(message)
 
         await websocket.send(json.dumps(message))
 
