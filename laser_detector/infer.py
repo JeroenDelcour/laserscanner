fix bug in loss calculation, change input resolution to 4:3 aspect ratio

laser_detector/infer.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import cv2
@@ -6,55 +7,67 @@ import torch
 from hourglassnet import HourglassNet
 from tqdm import tqdm
 
-checkpoint = Path("checkpoints") / "2023-11-10T16:13:51.380865.pt"
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("checkpoint", type=str, help="Path to Torch checkpoint")
+    parser.add_argument("video", type=str, help="Path to video")
+    return parser.parse_args()
 
-model = HourglassNet(nstack=2, inp_dim=256, oup_dim=1)
-model.to(device)
-model.load_state_dict(torch.load(checkpoint, map_location=device))
-model.eval()
 
-cap = cv2.VideoCapture("data/videos/2023-11-10_video0.mp4")
+def main(checkpoint, video):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
+    model = HourglassNet(nstack=1, inp_dim=256, oup_dim=1)
+    model.to(device)
+    model.load_state_dict(torch.load(checkpoint, map_location=device))
+    model.eval()
 
-input_size = (512, 512)
+    cap = cv2.VideoCapture(video)
 
-pbar = tqdm()
-while cap.isOpened():
-    success, frame = cap.read()
-    if not success:
-        break
+    cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
 
-    frame = cv2.resize(frame, dsize=input_size)
+    input_size = (384, 512)
 
-    model_input = np.moveaxis(frame, 2, 0)  # HWC to CHW
-    model_input = model_input[np.newaxis, :]  # batch dimension
-    model_input = model_input.astype(np.float32) / 255
-    model_input = torch.Tensor(model_input)
-    model_input = model_input.to(device)
+    pbar = tqdm()
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            break
 
-    with torch.no_grad():
-        outputs = model(model_input)
-    heatmap = outputs[0, 0, 0]
+        frame = cv2.resize(frame, dsize=input_size[::-1])
 
-    vis = heatmap.cpu().numpy()
-    vis = np.clip(vis, 0, 1)
-    vis *= 255
-    vis = vis.astype(np.uint8)
+        model_input = np.moveaxis(frame, 2, 0)  # HWC to CHW
+        model_input = model_input[np.newaxis, :]  # batch dimension
+        model_input = model_input.astype(np.float32) / 255
+        model_input = torch.Tensor(model_input)
+        model_input = model_input.to(device)
 
-    # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    vis = cv2.resize(vis, dsize=input_size)
-    vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
-    vis = np.hstack((frame, vis))
+        with torch.no_grad():
+            outputs = model(model_input)
+        heatmap = outputs[0, -1, 0]
 
-    cv2.imshow("frame", vis)
-    if cv2.waitKey(1) == ord("q"):
-        break
+        vis = heatmap.cpu().numpy()
+        vis = np.clip(vis, 0, 1)
+        vis *= 255
+        vis = vis.astype(np.uint8)
 
-    pbar.update()
+        # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        vis = cv2.resize(vis, dsize=input_size[::-1], interpolation=0)
+        vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
+        vis = np.hstack((frame, vis))
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow("frame", vis)
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+        pbar.update()
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(**vars(args))

laser_detector/train.py
@@ -14,11 +14,12 @@ from hourglassnet import HourglassNet
 
 torch.manual_seed(0)
 
-input_size = (512, 512)
-output_size = (128, 128)
+input_size = (384, 512)
+output_size = (96, 128)
 batch_size = 4
-epochs = 600
+epochs = 200
 num_workers = 4
+from_checkpoint = "checkpoints/2023-11-10T17:31:26.165278.pt"
 
 data_transforms = v2.Compose(
     [
@@ -56,14 +57,18 @@ def criterion(outputs, targets):
     """
     Calculate MSE loss for each hourglass stack and return the sum.
     """
-    loss = nn.functional.mse_loss(outputs[0], targets)
-    for output in outputs[1:]:
-        loss += nn.functional.mse_loss(output, targets)
+    n_stacks = outputs.size()[1]
+    loss = sum(
+        (nn.functional.mse_loss(outputs[:, i, 0], targets) for i in range(n_stacks))
+    )
     return loss
 
 
-model = HourglassNet(nstack=2, inp_dim=256, oup_dim=1)
+model = HourglassNet(nstack=1, inp_dim=256, oup_dim=1)
 model.to(device)
+
+if from_checkpoint:
+    model.load_state_dict(torch.load(from_checkpoint, map_location=device))
 
 optimizer = Adam(model.parameters(), lr=1e-4)
 