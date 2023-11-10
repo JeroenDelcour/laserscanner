43234a8e6280a9341ba2a4735d17e6056a5146f1
Jeroen Delcour <jeroendelcour@gmail.com>
2023-11-10 15:13:46 +0100

    much faster way to generate gaussians, add target shape option

--- laser_detector/dataloader.py
+++ laser_detector/dataloader.py
@@ -1,8 +1,8 @@
 import json
 from pathlib import Path
+from typing import Optional, Tuple
 
 import numpy as np
-import torch
 from matplotlib import pyplot as plt
 from PIL import Image, ImageOps
 from torch.utils.data import Dataset
@@ -10,12 +10,23 @@ from torch.utils.data import Dataset
 
 class SuperviselyLaserLineDataset(Dataset):
     def __init__(
-        self, supervisely_project_path: str, transforms=None, sigma=3, kernel_size=15
+        self,
+        supervisely_project_path: str,
+        transforms=None,
+        sigma: float = 1,
+        target_shape: Optional[Tuple[int, int]] = None,
     ):
+        """
+        Args:
+            supervisely_project_path: Path to Supervisely project directory.
+            transforms: Torchvision transforms.
+            sigma: Sigma of heatmap gaussians.
+            target_shape: Desired output heatmap shape (height, width). If None, the image size is used.
+        """
         self.data_path = Path(supervisely_project_path)
         self.transforms = transforms
         self.sigma = sigma
-        self.kernel_size = kernel_size
+        self.target_shape = target_shape
 
         self._image_paths = []
         self._ann_paths = []
@@ -25,45 +36,60 @@ class SuperviselyLaserLineDataset(Dataset):
             self._image_paths += sorted((datadir / "img").iterdir())
             self._ann_paths += sorted((datadir / "ann").iterdir())
 
-        x, y = np.meshgrid(
-            np.linspace(-kernel_size / 2, kernel_size / 2, kernel_size),
-            np.linspace(-kernel_size / 2, kernel_size / 2, kernel_size),
-        )
-        self._dst = np.sqrt(x**2 + y**2)
-
-    @profile
-    def _load_ann(self, ann_file):
+    def _load_ann(self, ann_file: str) -> np.ndarray:
         """
         Parses annotation JSON and generates a target heatmap.
         """
         with open(ann_file, "r") as f:
             ann = json.load(f)
-        target = np.zeros(
-            (ann["size"]["height"], ann["size"]["width"]), dtype=np.float32
-        )
-        mesh_x, mesh_y = np.meshgrid(
-            np.arange(ann["size"]["width"]),
-            np.arange(ann["size"]["height"]),
-        )
+        # generate gaussian distribution around line segments
+        if self.target_shape is not None:
+            mesh_x, mesh_y = np.meshgrid(
+                np.arange(self.target_shape[1]),
+                np.arange(self.target_shape[0]),
+            )
+            dst = np.zeros(
+                (self.target_shape[0], self.target_shape[1]), dtype=np.float32
+            )
+            x_scale = self.target_shape[1] / ann["size"]["width"]
+            y_scale = self.target_shape[0] / ann["size"]["height"]
+        else:
+            mesh_x, mesh_y = np.meshgrid(
+                np.arange(ann["size"]["width"]),
+                np.arange(ann["size"]["height"]),
+            )
+            dst = np.zeros(
+                (ann["size"]["height"], ann["size"]["width"]), dtype=np.float32
+            )
+            x_scale = 1
+            y_scale = 1
+        dst[:] = np.Infinity
         for obj in ann["objects"]:
             if not obj["geometryType"] == "line":
                 continue
             line_segments = zip(
                 obj["points"]["exterior"][:-1], obj["points"]["exterior"][1:]
             )
+            # calculate distance for each pixel to the nearest line segment
             for (x0, y0), (x1, y1) in line_segments:
+                x0 = x0 * x_scale
+                y0 = y0 * y_scale
+                x1 = x1 * x_scale
+                y1 = y1 * y_scale
                 if x0 > x1:
                     x0, y0, x1, y1 = x1, y1, x0, y0
-                for x in range(x0, x1 + 1):
-                    if x1 == x0:
-                        y = y0
-                    else:
-                        y = y0 + (x - x0) / (x1 - x0) * (y1 - y0)  # interpolate
-                    self._dst = np.sqrt((mesh_x - x) ** 2 + (mesh_y - y) ** 2)
-                    # generate gaussian
-                    gauss = np.exp(-((self._dst) ** 2 / (2 * self.sigma**2)))
-                    # paste into target heatmap
-                    target = np.maximum(target, gauss)
+                px = x1 - x0
+                py = y1 - y0
+                norm = px**2 + py**2
+                u = ((mesh_x - x0) * px + (mesh_y - y0) * py) / norm
+                u = np.clip(u, 0, 1)
+                x = x0 + u * px
+                y = y0 + u * py
+                dx = x - mesh_x
+                dy = y - mesh_y
+                segment_dst = 0.5 * (dx**2 + dy**2)
+                dst = np.minimum(dst, segment_dst)
+        target = np.exp(-((dst) ** 2 / (2 * self.sigma**2)))  # gaussian generation
         return target
 
     def __getitem__(self, index):
@@ -80,7 +106,9 @@ class SuperviselyLaserLineDataset(Dataset):
 
 if __name__ == "__main__":
     # some tests
-    dataset = SuperviselyLaserLineDataset("data/272599_Laser line detection")
+    dataset = SuperviselyLaserLineDataset(
+        "data/272599_Laser line detection", target_shape=(180, 320)
+    )
     image, target = dataset[0]
     fig, ax = plt.subplots(nrows=2)
     ax[0].imshow(image)
